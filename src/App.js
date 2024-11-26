504009f66663281de35c5436e217da4ef28ff37c
LDalessio <dalessiomlauren@gmail.com>
2024-11-25 17:53:38 -0700

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This website was coded by Lauren D'Alessio and is{" "}
+        <a
+          href="https://github.com/LDalessio/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
