379830c3240b3efd7a5993ea943e132ea069f812
LDalessio <dalessiomlauren@gmail.com>
2024-11-26 17:02:10 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This website was coded by Lauren D'Alessio and is{" "}
-        <a
-          href="https://github.com/LDalessio/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This website was coded by Lauren D'Alessio and is{" "}
+          <a
+            href="https://github.com/LDalessio/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
